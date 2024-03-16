2c571535904912ac49f0b4c7ed6918cf0f320c81
makhentosch <121441697+makhentosch@users.noreply.github.com>
2024-03-16 03:18:08 +0300

    defang.py

--- defang.py
+++ defang.py
@@ -13,9 +13,8 @@ def defang_ip_addresses(ip_addresses):
     defanged_ips = [defang_ip(ip) for ip in ip_addresses]
     return defanged_ips
 
-# Example usage:
-print('Exapmle: 192.168.1.1 10.0.0.1:8080 8.8.8.8 2001:0000:130F')
-input_ip_addresses = input('[*]Enter the IP addresses separated by spaces like shown above: ')
+
+input_ip_addresses = input('[*]Enter the IP addresses: ')
 input_ip_addresses = input_ip_addresses.split(' ')
 defanged_ips = defang_ip_addresses(input_ip_addresses)
 
